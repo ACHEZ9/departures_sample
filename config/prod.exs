f82db386d48c0ac8cd0a9aa5e9cc9df8663f8ce0
Allan James Chesarone <achesarone@Allans-MacBook-Pro.local>
2018-02-02 09:59:09 -0500

    heroku config copy error

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :mbta_sample, MbtaSampleWeb.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello, Hello.Repo,
+config :mbta_sample, MbtaSample.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
